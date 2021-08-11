01273c19ac17719961a0ad42314a4cdbd03fce89
Nikolay Malyutin <xenos-nick@mail.ru>
2021-08-11 19:55:17 +0300

    corrections were made to the code in the file main.py

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ with open("recipes.txt", encoding="utf-8") as file:
             ingredients.append({'ingredient_name': name, 'quantity': int(quantity), 'measure': measure})
             cook_book[dish_name] = ingredients
         file.readline()
+    file.close()
 
 
 def dish_add():
@@ -22,22 +23,26 @@ def person_count_input():
     return count
 
 
-def get_shop_list_by_dishes(dishes, person_count):
-    shop_list = {}
+def get_shop_list_by_dishes(dishes, persons):
+    ingredient_list = {}
     for dish in dishes:
         if dish.capitalize() in list(cook_book.keys()):
             for ingredient in cook_book[dish.capitalize()]:
-                if ingredient['ingredient_name'] in list(shop_list.keys()):
-                    ingredient['quantity'] += shop_list[ingredient['ingredient_name']]['quantity'] / person_count
-                shop_list[ingredient['ingredient_name']] = {'measure': ingredient['measure'],
-                                                            'quantity': round(ingredient['quantity']) * person_count}
+                if ingredient['ingredient_name'] in list(ingredient_list.keys()):
+                    ingredient['quantity'] += ingredient_list[ingredient['ingredient_name']]['quantity'] / persons
+                ingredient_list[ingredient['ingredient_name']] = {'measure': ingredient['measure'],
+                                                                  'quantity': round(ingredient['quantity']) * persons}
+
         else:
-            print('Рецепт не известен')
-    pprint(shop_list)
+            print(f'Рецепт {dish.capitalize()} не известен')
+            return
+    return ingredient_list
 
 
-dishes = dish_add()
+dishes_to_cooking = dish_add()
 
 person_count = person_count_input()
 
-get_shop_list_by_dishes(dishes, person_count)
+shop_list = get_shop_list_by_dishes(dishes_to_cooking, person_count)
+
+print(shop_list)
